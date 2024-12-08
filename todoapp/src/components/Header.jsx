d1b2e7105720c55edad82db375a369f7c761bacf
Sava <savasamuel217@gmail.com>
2024-12-08 22:45:33 +0100

    second commit

---
+++ todoapp/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav>
+        <h1 className='flex justify-center items-center text-2xl font-bold pt-5'>
+            To do list
+        </h1>
+    </nav>
+  )
+}
+
+export default Header
